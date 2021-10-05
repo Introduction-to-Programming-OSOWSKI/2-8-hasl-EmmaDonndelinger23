786df2c4d068d4343775ceffd744b1e0c33c3640
EmmaDonndelinger23 <826656@mystma.org>
2021-10-05 14:53:52 -0500

    2

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ def hasL(w):
     for i in range(0, len(w)):
         if w[i] == "l":
            return True
-print(hasL("chocolate"))
+        else:
+            return False
+print(hasL("chocoate"))
